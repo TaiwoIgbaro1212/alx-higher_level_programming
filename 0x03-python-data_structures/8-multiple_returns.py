81451c33323af6ff49a08469cd4914b5cfa053c8
TaiwoIgbaro1212 <igbaro.paul@gmail.com>
2022-12-05 00:17:04 -0800

    task all

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-# 8-multiple_returns.py
 
 def multiple_returns(sentence):
-    """
-    Returns the length of a string and its first character.
-    """
-    if sentence == "":
-        return (0, None)
-    return (len(sentence), sentence[0])]
+    if sentence != '':
+        first_char = sentence[0]
+    else:
+        first_char = None
+    return (len(sentence), first_char)
